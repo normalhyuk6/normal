7c8223140e241c8f18730a5c246e14321120e4e2
normalhyuk6 <normalhyuk6@gmail.com>
2024-06-14 08:12:16 +0900

    Add files via upload

---
+++ home_effect/script.js
@@ -0,0 +1,11 @@
+/**
+  * Purely for restarting the animation 
+*/
+const button = document.querySelector('button');
+const restart = () => {
+  const blocks = document.querySelector('.block-reveal');
+  const newBlocks = blocks.cloneNode(true);
+  document.body.removeChild(blocks);
+  document.body.appendChild(newBlocks);
+};
+button.addEventListener('click', restart);
